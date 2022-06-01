1b1680c77d63da5203c90a92bce31041e1b69bcb
easyhyepy <jhlee1326@naver.com>
2022-06-01 12:51:48 +0900

    5/31까지의 코드 총정리 -> 클래스다이어그램과의 연결확인 & 함수 리턴 void를 int로 형변환 & OperationIndicator 클래스 추가 & 구현해야할 부분 TODO로 표시함 (빠진 거 있을 수 있음) & 앞으로 시퀀스 다이어그램 체크하면서 흐름 재확인 할 예정

--- vendingMachine/src/vendingMachine/VendingMachine.java
+++ vendingMachine/src/vendingMachine/VendingMachine.java
@@ -26,6 +26,8 @@ public class VendingMachine {
 	
 }
 
+
+
 class UserPanel {
 	int cash;
 	String selection;
@@ -37,20 +39,15 @@ class UserPanel {
 		this.selection = selection;
 		Controller c = new Controller();
 		c.getUserInput(this.cash, this.selection);
-		
-		// System.out.println("c출력입니다"); 		c.getCash();		c.getSelection();
+		//System.out.print("c출력입니다: "); 		c.getCash();	System.out.print(" ");  	c.getSelection();		//TODO 의외로 accpet가 맨뒤에 출력되네???
 		
 	}
-	
-	void setDisplayPharase(String displayPharase) { this.displayPharase = displayPharase; }
-	//void receiveCoffee(String selection)
-	//void receiveChange(String change);
 
+	void setDisplayPharase(String displayPharase) { this.displayPharase = displayPharase; }
 	void getCash() { System.out.println(this.cash); }
 	void getSelection() { System.out.println(this.selection); }
-	void getDisplayPharase() { System.out.println(this.displayPharase); }
-	
-	
+	String getDisplayPharase() { return (this.displayPharase); }
+
 	
 	void receiveCoffee(String selection) {
 		System.out.print(selection); System.out.println(" 커피가 나왔다.");
@@ -58,8 +55,28 @@ class UserPanel {
 		//m.setFinishFalse();
 	}
 	
-	void change(int change) { System.out.print(change); System.out.println("원의 잔돈이 UserPanel에 반환되었다.");}
+	void receiveChange(int change) { 
+		System.out.print(change); System.out.println("원의 잔돈이 UserPanel에 반환되었다.");
+	}
 }
+
+
+
+class OperationIndicator {		//TODO 클래스 다이어그램 내용인데 시퀀스에도 추가해야함~
+	boolean LEDStatus = false;
+	
+	void turnLED_On() {
+		LEDStatus = true;
+	}
+	void turnLED_Off() {
+		LEDStatus = false;
+	}
+	boolean getLEDStatus() {
+		return this.LEDStatus;
+	}
+}
+
+
 
 class Controller {
 
@@ -72,9 +89,10 @@ class Controller {
 		
 		MoneyManager m = new MoneyManager();
 		//System.out.println("잔액있는지, 그리고 투입금액 이상인지 확인"); 		System.out.println(m.checkAvailibilityOfChangesAndAboveprice(this.cash, this.selection));
-		if (m.checkAvailibilityOfChangesAndAboveprice(this.cash, this.selection)) {
+		
+		if (m.checkAvailibilityOfChangesAndAbovePrice(this.cash, this.selection)) {
 			m.updateBalance(cash,selection);
-			m.getBalance();
+			m.getBalance();		System.out.println();//자판기 잔돈 체크함. 
 			
 			CupManager cm = new CupManager();
 			IngredientManager im = new IngredientManager();
@@ -83,20 +101,30 @@ class Controller {
 			if (cm.checkAvailibility() && im.checkAvailibility(selection) && wm.checkAvailibility(selection)) {
 				Manufacture mf = new Manufacture();
 				System.out.print("Manufacture 체크: ");mf.getRequestManufacture(this.selection);
-			}
-			
+			}	
 		}
-		else {System.out.println("sys balance 업데이트 안됨. 즉 사용자가 입력한 금액 문제있는겨 -> 오류 메시지 띄워야"); m.getBalance();}
-	
+		
+		else {
+			System.out.println("sys balance 업데이트 안됨. 즉 사용자가 입력한 금액 문제있는겨 -> 오류 메시지 띄워야");
+			m.getBalance();
+		}					//TODO 오류메시지
 		
 	}
 	
 	//void getRecipe(String selection);
-
-	void getCash() { System.out.println(this.cash); }
-	void getSelection() { System.out.println(this.selection); }
+	int getCash() {
+		System.out.print(this.cash);
+		return this.cash;
+	}			//첫코딩때 void였는데 바꿨음. 근데 6/1오전에 출력해보니 c출력입니다가 맨 뒤에 출력되더라?
+	
+	String getSelection() {
+		System.out.println(this.selection);
+		return this.selection;
+	}
 	
 }
+
+
 
 class MoneyManager {
 	int balance=4000;	//잔액
@@ -105,7 +133,7 @@ class MoneyManager {
 	//boolean notFinish = true;
 	//void setFinishFalse() {this.notFinish = false;}
 	
-	boolean checkAvailibilityOfChangesAndAboveprice(int cash, String selection) {		//잔액만 확인하는게 아니라 투입금액>=물품가액도 확인해야 & 마지막 else부분은 없는 문자열로 잘못 입력한겅미
+	boolean checkAvailibilityOfChangesAndAbovePrice(int cash, String selection) {		//잔액만 확인하는게 아니라 투입금액>=물품가액도 확인해야 & 마지막 else부분은 없는 문자열로 잘못 입력한겅미
 		if (selection.equals("SpecialCoffee")) {
 			if(cash>=2000) {
 				availibility = (balance>=cash-2000)?true:false;
@@ -137,19 +165,20 @@ class MoneyManager {
 		//while (notFinish){  //MoneyManager의 notFinish와 setFinishiFalse()로 잔돈 반환을 늦추려했으나 실패. 그래서 걍 주석해두겠음. UserPanel의 setFinishFalse()도 마찬가지로 주석해둠
 		UserPanel u = new UserPanel();
 
-		if (selection.equals("SpecialCoffee")) { u.change(cash-2000); }
-		else if (selection.equals("PlainCoffee")) { u.change(cash-1000); }
-		else if (selection.equals("BlackCoffee")) { u.change(cash-1500); }
-
-		//}
-		
+		if (selection.equals("SpecialCoffee")) { u.receiveChange(cash-2000); }
+		else if (selection.equals("PlainCoffee")) { u.receiveChange(cash-1000); }
+		else if (selection.equals("BlackCoffee")) { u.receiveChange(cash-1500); }
 	}
-	void getBalance() { System.out.print("자판기 잔고확인: "); System.out.println(this.balance); }
+	
+	int getBalance() { System.out.print("자판기 잔고확인: "); System.out.println(this.balance); return this.balance;}		//void에서 int로 바꿈: 컨트롤러의 m.getBalance();에서 작동됨
 		
 }
 
+
+
 class CupManager {
 	int cupCount = 10;
+	
 	boolean checkAvailibility() {
 		if (cupCount>=1) return true;
 		else {return false;}
@@ -161,12 +190,13 @@ class CupManager {
 	}
 }
 
+
+
 class IngredientManager {
-	int SpecialCoffeeCount = 1;
+	int SpecialCoffeeCount = 1;		//하나로 퉁쳐서 count하는 것은 말이 안돼서 추가했음.
 	int PlainCoffeeCount = 5;
 	int BlackCoffeeCount  = 10;
-	//String selection;
-	
+
 	boolean checkAvailibility(String selection) {
 		if (SpecialCoffeeCount>=1) return true;
 		else if (PlainCoffeeCount>=1) return true;
@@ -175,7 +205,6 @@ class IngredientManager {
 	}
 	
 	int getOrderIngredient(String selection) {
-		//this.selection=selection;
 		System.out.println("IngredientManager는 Manufacture에게 Ingredient을 준다.");
 		if (selection.equals("SpecialCoffee")) { SpecialCoffeeCount--; }
 		else if (selection.equals("PlainCoffee")) { PlainCoffeeCount--; }
@@ -189,6 +218,8 @@ class IngredientManager {
 	int getBlackCoffeeCount() {return BlackCoffeeCount;}
 }
 
+
+
 class WaterManager {
 	int amountOfWater=1000;	//ml단위
 	
@@ -196,13 +227,16 @@ class WaterManager {
 		if (amountOfWater>=150) return true;		//레시피 따라서 해도 될듯
 		else {return false;}
 	}
-	
 	int getOrderWater() {
 		amountOfWater-=150;
 		return 150;
 	}
-	int getAmountOfWater() {return amountOfWater;}
+	int getAmountOfWater() {
+		return amountOfWater;
+	}
 }
+
+
 
 class Manufacture {
 	String selection;
@@ -224,17 +258,26 @@ class Manufacture {
 		System.out.print(wm.getAmountOfWater());		System.out.println("ml의 물이 WaterManger에 남아있다.\n");
 		
 		CoffeeDispenser cd = new CoffeeDispenser();
-		cd.getMixed(selection);
+		cd.getMixed(selection, cm.getOrderCup(), im.getOrderIngredient(this.selection), wm.getOrderWater());		//selection만 있었는데 추가함
 		
 	}
 }
 
+
+
 class CoffeeDispenser {
-	void getMixed(String selection) { 
-		System.out.println("CoffeeDispenser은 컵과 재료와 물을 받았고, 이를 섞는다.\n");
+	boolean cup = false;
+	boolean ingredient = false;
+	boolean water = false;
+	
+	void getMixed(String selection, int cup, int ingredient, int water) { 
+		this.cup = true;
+		this.ingredient =true;
+		this.water = true;
+		
+		System.out.println("\nCoffeeDispenser은 컵과 재료와 물을 받았고, 이를 섞는다.\n");
 		UserPanel u = new UserPanel(); 
 		u.receiveCoffee(selection);
 		
 	}
-	
 }
