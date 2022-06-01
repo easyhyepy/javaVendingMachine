842cb2699d4c9dcf5a39b58a15a9c0d269befec0
easyhyepy <jhlee1326@naver.com>
2022-06-02 04:27:27 +0900

    최종_깔끔히 정리한 코드

--- vendingMachine/src/vendingMachine/VendingMachine.java
+++ vendingMachine/src/vendingMachine/VendingMachine.java
@@ -48,7 +48,8 @@ class UserPanel {
 		this.cash = cash;
 		this.selection = selection;
 		
-		System.out.println("\n\n\n\n왼쪽은 사용자가 보는 자판기입니다.\t\t\t\t오른쪽은 사용자에게 보이지 않는 중간 동작 과정입니다.\n------------------------------------------------------------------------------------------------------------");
+		System.out.println("\n\n\n\n왼쪽은 사용자가 보는 자판기입니다.\t\t\t\t오른쪽은 사용자에게 보이지 않는 중간 동작 과정입니다."
+				+ "\n------------------------------------------------------------------------------------------------------------");
 		System.out.println("사용자가 돈을 넣고 선택했다.");
 		System.out.println("\t\t\t\t\t\t\tUserPanel은 돈을 받았다.");
 		Controller c = new Controller();		  //UserPanel u = new UserPanel();
@@ -62,7 +63,6 @@ class UserPanel {
 	String getSelection() { return this.selection;}
 	String getDisplayPharase() { return (this.displayPharase); }
 
-	
 	void receiveCoffee(String selection) {
 		System.out.print(selection); System.out.println(" 커피가 나왔다.");
 		//MoneyManager m = new MoneyManager();
@@ -88,7 +88,7 @@ class Controller {
 		
 		System.out.println("\t\t\t\t\t\t\tUserPanel이 돈을 Controller에게 전달했다. Controller은 돈을 받았다.");
 		MoneyManager m = new MoneyManager(4000);			//생성자-> 4000전달
-		//System.out.println("잔액있는지, 그리고 투입금액 이상인지 확인"); 		System.out.println(m.checkAvailibilityOfChangesAndAboveprice(this.cash, this.selection));
+		
 		
 		if (m.checkAvailibilityOfChangesAndAbovePrice(this.cash, this.selection)) {
 			
@@ -98,11 +98,11 @@ class Controller {
 			CupManager cm = new CupManager(10);		//생성자 -> 10전달
 			IngredientManager im = new IngredientManager(5, 10, 0);		//생성자 -> 1, 5, 10 전달이였는데 NoChange를 위해 5, 10, 0으로 함
 			WaterManager wm = new WaterManager(1000);		//생성자 -> 1000(ml) 전달
-			//System.out.println("manager들(cm, im, wm)의 체크"); 			System.out.println(cm.checkAvailibility());			System.out.println(im.checkAvailibility(selection));			System.out.println(wm.checkAvailibility(selection));
+			
 			if (cm.checkAvailibility() && im.checkAvailibility(selection, u, cash) && wm.checkAvailibility(selection)) {
 				
 				//m.updateBalance(cash, selection);
-				//m.getBalance();		System.out.println();//자판기 잔돈 체크함. 		//위 주석 코드에서 바꾸..려다가 안바꿔도 되는 것 알게됨
+				//m.getBalance();
 				
 				//operation Indicatotor 불켜지게 하는 코드 추가
 				OperationIndicator oi = new OperationIndicator();
@@ -114,24 +114,19 @@ class Controller {
 				mf.getRequestManufacture(this.selection, oi, u, m ,cm, im, wm);
 				
 			}
-			
-			
 		}
 		
 		else {
 			System.out.println("---NoChage---");
-			//System.out.println("sys balance 업데이트 안됨. 즉 사용자가 입력한 금액 문제있는겨 -> 오류 메시지 띄워야");
-			//m.getBalance();
 			u.receiveChange(cash);
 		}
 		
 	}
 	
-	//void getRecipe(String selection);
 	int getCash() {
 		System.out.print(this.cash);
 		return this.cash;
-	}			//첫코딩때 void였는데 바꿨음. 근데 6/1오전에 출력해보니 c출력입니다가 맨 뒤에 출력되더라?
+	}
 	
 	String getSelection() {
 		System.out.println(this.selection);
@@ -142,7 +137,7 @@ class Controller {
 
 
 
-class OperationIndicator {		//TODO 클래스 다이어그램 내용인데 시퀀스에도 추가해야함~
+class OperationIndicator {
 	boolean LEDStatus = false;
 	
 	void turnLED_On() {
@@ -169,7 +164,7 @@ class MoneyManager {
 		this.balance=balance;
 	}
 	
-	boolean checkAvailibilityOfChangesAndAbovePrice(int cash, String selection) {		//잔액만 확인하는게 아니라 투입금액>=물품가액도 확인해야 & 마지막 else부분은 없는 문자열로 잘못 입력한겅미
+	boolean checkAvailibilityOfChangesAndAbovePrice(int cash, String selection) {	//잔액만 확인하는게 아니라 투입금액>=물품가액도 확인해야 & 마지막 else부분은 없는 문자열로 잘못 입력한겅미
 		if (selection.equals("SpecialCoffee")) {
 			if(cash>=2000) {
 				availibility = (balance>=cash-2000)?true:false;
@@ -208,7 +203,6 @@ class MoneyManager {
 	}
 	
 	void getFinsihCoffee(String selection, UserPanel u) {
-		//UserPanel U = new UserPanel();	
 		int cash = u.getCash();
 		if (selection.equals("SpecialCoffee")) { u.receiveChange(cash-2000); }
 		else if (selection.equals("PlainCoffee")) { u.receiveChange(cash-1000); }
